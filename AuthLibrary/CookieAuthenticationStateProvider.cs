476c7f40c00054f531b93291d1533580390f2368
Adam O'Neil <adamosoftware@gmail.com>
2024-05-27 10:16:01 -0400

    comment on cookie auth state

--- AuthLibrary/CookieAuthenticationStateProvider.cs
+++ AuthLibrary/CookieAuthenticationStateProvider.cs
@@ -8,6 +8,8 @@ namespace AuthLibrary;
 
 /// <summary>
 /// based on https://github.com/dotnet/blazor-samples/blob/main/8.0/BlazorWebAssemblyStandaloneWithIdentity/BlazorWasmAuth/Identity/CookieAuthenticationStateProvider.cs
+/// I did this to address an issue with PersistentAuthenticationStateProvider -- it appeared that its PersistentComponentState.TryTakeFromJson is not working
+/// for some reason, so I went this route. However while this one works, it doesn't address the root issue.
 /// </summary>
 public abstract class CookieAuthenticationStateProvider<TUserInfo>(HttpClient httpClient, ILogger<CookieAuthenticationStateProvider<TUserInfo>> logger) : AuthenticationStateProvider
 {
