57529609a9b4871de86a143b23914bb3bbb341ad
Adam O'Neil <adamosoftware@gmail.com>
2024-06-02 10:18:18 -0400

    see current render mode

---
+++ Application/Application.Client/Extensions/CurrentRenderMode.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Client.Extensions;
+
+public enum RenderModeOptions
+{
+	Server,
+	WebAssembly
+}
+
+public static class CurrentRenderMode
+{
+	/// <summary>
+	/// https://stackoverflow.com/a/77703444/25057433
+	/// </summary>
+	public static RenderModeOptions Value => OperatingSystem.IsBrowser() ? RenderModeOptions.WebAssembly : RenderModeOptions.Server;
+}
