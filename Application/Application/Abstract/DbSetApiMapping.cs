0643c5635ec0d91d1e144a4f33130c9bbd9235c3
Adam O'Neil <adamosoftware@gmail.com>
2024-06-04 07:22:08 -0400

    moved DbSetApiMapping back into App project

---
+++ Application/Application/Abstract/DbSetApiMapping.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application;
+
+internal abstract class DbSetApiMapping<TDbContext, TEntity, TKey>(string pattern)
+	where TDbContext : DbContext
+	where TEntity : class
+	where TKey : struct
+{
+	private readonly string _pattern = pattern;
+
+	protected abstract Task<IEnumerable<TEntity>> GetAllAsync(TDbContext dbContext, HttpContext httpContext);
+
+	protected abstract Task<TEntity?> GetAsync(TDbContext dbContext, HttpContext httpContext, TKey id);
+
+	protected abstract Task DeleteAsync(TDbContext dbContext, HttpContext httpContext, TKey id);
+
+	protected abstract void InsertOrUpdate(TDbContext dbContext, HttpContext httpContext, TEntity entity);
+
+	public void Map(IEndpointRouteBuilder routeBuilder)
+	{
+		routeBuilder.MapGet(_pattern, async (TDbContext db, HttpContext context) =>
+		{
+			var results = await GetAllAsync(db, context);
+			return Results.Ok(results);
+		});
+
+		routeBuilder.MapGet($"{_pattern}/{{id}}", async (TDbContext db, HttpContext context, TKey id) =>
+		{
+			var result = await GetAsync(db, context, id);
+			if (result is null) return Results.NotFound();
+			return Results.Ok(result);
+		});
+
+		routeBuilder.MapPost(_pattern, async (TDbContext db, HttpContext context) =>
+		{
+			var entity = await context.Request.ReadFromJsonAsync<TEntity>() ?? throw new Exception($"couldn't parse json as type {typeof(TEntity).Name}");
+			InsertOrUpdate(db, context, entity);
+			await db.SaveChangesAsync();
+			return Results.Ok(entity);
+		});
+
+		routeBuilder.MapDelete($"{_pattern}/{{id}}", async (TDbContext db, HttpContext context, TKey id) =>
+		{
+			await DeleteAsync(db, context, id);
+			return Results.Ok();
+		});
+	}
+}
